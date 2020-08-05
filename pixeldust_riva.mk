de0445b9d6a63a0d586d134c51162d6239d7a7d7
Zahar45 <zaharbush88@gmail.com>
2020-08-05 13:44:24 +0000

    riva: bringup pixeldust

--- pixeldust_riva.mk
+++ pixeldust_riva.mk
@@ -20,8 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelDust stuff
+$(call inherit-product, vendor/pixeldust/configs/pixeldust_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_DEVICE := riva
+TARGET_EXCLUDE_QCOM_VENDOR_SEPOLICY := true
+
+# Include optional stuff (e.g. prebuilt apps)
+include vendor/pixeldust/configs/system_optional.mk
 
 # Inherit from riva device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -30,7 +36,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := pixeldust_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
